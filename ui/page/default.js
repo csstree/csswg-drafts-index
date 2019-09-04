741fe2080ca8240d0a9ecdfe2cec35075502d844
Roman Dvornov <rdvornov@gmail.com>
2019-09-04 17:53:51 +0300

    Extend default page with reports

--- ui/page/default.js
+++ ui/page/default.js
@@ -1,11 +1,13 @@
 /* global discovery */
 
 discovery.page.define('default', [
-    'h1:#.name',{
+    'h1:#.name',
+    {
         view: 'context',
         data: [
-            { title: 'Specs', query: 'specs' },
-            { title: 'Definitions', query: 'defs' }
+            { title: 'Specs', query: 'specs.sort(<props.title>)' },
+            { title: 'Definitions', query: 'defs' },
+            { title: 'Problem syntaxes', query: 'defs.[definitionSyntax.value.error]' }
         ],
         content: {
             view: 'inline-list',
@@ -13,8 +15,29 @@ discovery.page.define('default', [
             data: `.({
                 label: title,
                 value: query.query(#.data, #).size(),
-                href: href or pageLink('report', { query, title })
+                href: href or pageLink('report', { title, query, view })
             })`
         }
+    },
+    {
+        view: 'section',
+        header: 'text:"Reports"',
+        content: {
+            view: 'ul',
+            item: 'link:{ text: title, href: pageLink("report", { ..., noedit: true }) }'
+        },
+        data: [
+            {
+                title: 'Specs and statuses',
+                query: 'specs.sort(<props.title>)',
+                view: '{\n    view: \'ol\',\n    limit: false,\n    item: [\'auto-link\', \'badge:props.status\']\n}'
+            },
+            {
+                title: 'Problem syntaxes',
+                noedit: true,
+                query: 'defs.group(<definitionSyntax.value.error>).[key]',
+                view: '{\n    view: \'list\',\n    item: {\n        view: \'expand\',\n        expanded: true,\n        title: \'pre:key\',\n        content: {\n            view: \'list\',\n            data: \'value\',\n            item: \'def\'\n        }\n    }\n}'
+            }
+        ]
     }
 ]);
